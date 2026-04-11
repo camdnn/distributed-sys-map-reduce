f4fb509dbbae7180d445d86d4d4c29fd1b1decc5
camdnn <camden.mann0105@gmail.com>
2026-04-10 20:20:49 -0700

    added reduce function, opening read files and write files, inf for loop for work and output file writing

--- worker/client.go
+++ worker/client.go
@@ -15,32 +15,66 @@ import (
 func main() {
 
 	client, _ := rpc.Dial("tcp", "localhost:7777")
-
 	request := common.Request{}
-
-	// add R value for response struct
 	var response = common.Response{}
 
-	client.Call("CoordinatorAPI.requestTask", request, &response)
+	for {
+		if err := client.Call("CoordinatorAPI.RequestTask", request, &response); err != nil {
+			log.Fatal("unable to connect to server:", err)
+		}
+		fmt.Printf("Response from server: %v\n", response)
 
-	if response.Task.TaskType == "M" {
-		kv := make(map[string]int)
-		mapping(response, kv)
-	} else {
+		if response.Task.TaskType == "M" {
+			// init intermediate files into a list
+			files := openFilesForWriting(&response)
+			if files == nil {
+				log.Fatal("failed to init any intermediate files")
+			}
 
+			// create kv pairs
+			kv := make(map[string]int)
+			if err := mapping(&response, kv); err != nil {
+				log.Fatal("Failed to map key value")
+			}
+
+			// write to the files
+			err := writeToFile(files, kv, response.Task.R)
+			if err != nil {
+				log.Fatal("failed to open file %w", err)
+			}
+
+			clear(kv) //empties map, same allocation
+
+		} else if response.Task.TaskType == "R" {
+
+			// init intermediate files into a list
+			filesWrite := openFilesForReading(&response)
+			if filesWrite == nil {
+				log.Fatal("failed to init any intermediate files")
+			}
+
+			reduced := make(map[string]int)
+			if err := reducer(reduced, filesWrite); err != nil {
+				log.Fatal("Error in reducer")
+			}
+
+			if err := commitFiles(reduced, response.Task.Filename); err != nil {
+				log.Fatal("Error in commiting Files")
+			}
+		} else {
+			log.Println("Error not valid task type")
+		}
 	}
-
-	fmt.Printf("Response from server: %v\n", response)
 }
 
+// MAPPING
 // Mapping function
 // recieve a response, and a kv pair
-func mapping(r common.Response, kv map[string]int) error {
+func mapping(r *common.Response, kv map[string]int) error {
 	// open the map function
 	file, err := os.Open(r.Task.Filename)
 	if err != nil {
 		log.Println("OpenFile: ", err)
-		return err
 	}
 	defer file.Close()
 
@@ -52,43 +86,81 @@ func mapping(r common.Response, kv map[string]int) error {
 			kv[word]++
 		}
 	}
+	return nil
+}
 
-	for key, value := range kv {
-		fmt.Println("Key: ", key)
-		fmt.Println("Value: ", value)
-	}
-
-	// init intermediate files into a list
-	files := initFiles(r.Task.R)
-	if files == nil {
-		log.Fatal("failed to init any intermediate files")
-	}
-
-	// clean all files when done
-	defer func() {
-		for _, f := range files {
-			if f != nil {
-				f.Close()
+// reduced the kv pairs in the intermediate files
+func reducer(reduced map[string]int, files []*os.File) error {
+	// for each intermediate file decode the kv pairs and accumulate them
+	for i := range files {
+		decoder := json.NewDecoder(files[i])
+		for decoder.More() {
+			var kv KV
+			if err := decoder.Decode(&kv); err != nil {
+				return fmt.Errorf("decode error: %w", err)
 			}
+			reduced[kv.Key] += kv.Value
 		}
-	}()
+	}
 
-	err = writeToFile(files, kv, r.Task.R)
-	if err != nil {
-		return fmt.Errorf("failed to open file %w", err)
+	// close all files after reading
+	for _, f := range files {
+		if err := f.Close(); err != nil {
+			return fmt.Errorf("close intermediate file: %w", err)
+		}
 	}
 
 	return nil
+}
 
+// Commit files to final output file
+func commitFiles(reduced map[string]int, outputPath string) error {
+	file, err := os.OpenFile(outputPath, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		return fmt.Errorf("open output file error: %w", err)
+	}
+	defer file.Close()
+
+	outFile := json.NewEncoder(file)
+	for key, value := range reduced {
+		if err := outFile.Encode(KV{Key: key, Value: value}); err != nil {
+			fmt.Println("error encoding key value pair")
+		}
+	}
+
+	return nil
 }
 
 // initialize intermediate files for wokers
-func initFiles(R int) []*os.File {
-	files := make([]*os.File, R)
-	for i := range R {
-		filename := fmt.Sprintf("intermediate_%d.json", i)
+func openFilesForWriting(r *common.Response) []*os.File {
+	files := make([]*os.File, r.Task.R)
+	for i := range r.Task.R {
+		filename := fmt.Sprintf("mr-%d-%d.json", r.Task.TaskID, i)
 
 		fd, err := os.OpenFile(filename, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+		if err != nil {
+
+			// clean up already opened files before returning
+			for j := range i {
+				files[j].Close()
+			}
+			log.Println("Failed to open intermediate file: ", i, err)
+			return nil
+		}
+
+		files[i] = fd
+	}
+	return files
+}
+
+// initialize intermediate files for wokers
+// // R = # of map
+func openFilesForReading(r *common.Response) []*os.File {
+	files := make([]*os.File, r.Task.R)
+	for i := range r.Task.R {
+		filename := fmt.Sprintf("mr-%d-%d.json", i, r.Task.TaskID)
+
+		fd, err := os.Open(filename)
 		if err != nil {
 
 			// clean up already opened files before returning
@@ -118,6 +190,7 @@ func writeToFile(files []*os.File, kv map[string]int, R int) error {
 		encoders[i] = json.NewEncoder(file)
 	}
 
+	// for each key valye get a has key for the file and print to that file
 	for key, value := range kv {
 		fileNo := ihash(key) % R
 		err := encoders[fileNo].Encode(KV{Key: key, Value: value})
@@ -125,8 +198,14 @@ func writeToFile(files []*os.File, kv map[string]int, R int) error {
 			return fmt.Errorf("failed to encode key value pair: %w", err)
 		}
 	}
-	return nil
 
+	// close all files after writing — reducer will re-open by name
+	for _, f := range files {
+		if err := f.Close(); err != nil {
+			return fmt.Errorf("close intermediate file: %w", err)
+		}
+	}
+	return nil
 }
 
 // hash func for intermediate files
