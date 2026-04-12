7d6f414371e5f90692713d5d274975de3a0cc355
Roan Morgan <roanlm007@gmail.com>
2026-04-12 15:14:49 -0700

    same as last commit

--- common/data.go
+++ common/data.go
@@ -2,12 +2,12 @@ package common
 
 // all information about a specific task
 type Task struct {
-	TaskId     int
-	TaskType   string // M or R
-	InProgress bool
-	Filename   string
-	R          int
-	M          int
+	TaskId   int
+	TaskType string // M or R
+	Done     bool
+	Filename string
+	R        int
+	M        int
 }
 
 // from worker
